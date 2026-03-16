009438b18e0d882b9178f4f0b8036c73380bf0c6
Shadman Arko <35429182+shadmanArko@users.noreply.github.com>
2026-03-16 15:26:54 +0100

    implement Serialize and Deserialize in GraphSerializer
    Implemented Serialize method for converting graph objects into a storable format.
    Implemented Deserialize method for reconstructing graph objects from serialized data.
    Centralized graph serialization logic within the GraphSerializer class.

--- Assets/_Scripts/Serialization/GraphSerializer.cs
+++ Assets/_Scripts/Serialization/GraphSerializer.cs
@@ -1,16 +1,120 @@
 using System;
+using System.Collections.Generic;
 using _Scripts.Blocks;
 using _Scripts.Core;
+using _Scripts.Models;
+using _Scripts.Services.Factory;
+using Cysharp.Threading.Tasks;
 using UnityEngine;
 
 namespace _Scripts.Serialization
 {
     public static class GraphSerializer
     {
-        //todo Serialize
-        
-        //todo Deserialize
-        
+        public static GraphData Serialize(IGraphModel model)
+        {
+            var data = new GraphData
+            {
+                version = "2.0",
+                exportedAt = DateTime.UtcNow.ToString("o")
+            };
+
+            foreach (var block in model.Blocks)
+            {
+                if (block == null) continue;
+                data.blocks.Add(new BlockData
+                {
+                    blockId = block.blockId,
+                    blockType = block.BlockType,
+                    gameObjectName = block.name,
+                    position = new SerializableVector3(block.transform.position),
+                    propertiesJson = SerializeProps(block)
+                });
+            }
+
+            foreach (var conn in model.Connections)
+            {
+                data.connections.Add(new ConnectionData
+                {
+                    fromBlockId = conn.fromBlockId,
+                    fromPortName = conn.fromPortName,
+                    toBlockId = conn.toBlockId,
+                    toPortName = conn.toPortName
+                });
+            }
+
+            foreach (var go in model.GetRuntimeObjects())
+            {
+                if (go == null) continue;
+                string primType = "Cube";
+                var mf = go.GetComponent<MeshFilter>();
+                if (mf?.sharedMesh != null)
+                    primType = mf.sharedMesh.name.Replace(" Instance", "");
+
+                data.runtimeObjects.Add(new RuntimeObjectData
+                {
+                    name = go.name,
+                    primitiveType = primType,
+                    position = new SerializableVector3(go.transform.position),
+                    rotation = new SerializableVector3(go.transform.eulerAngles),
+                    scale = new SerializableVector3(go.transform.localScale)
+                });
+            }
+
+            return data;
+        }
+
+
+        public static async UniTask DeserializeAsync(GraphData data, IGraphModel model)
+        {
+            model.ClearRuntimeObjects();
+            var toDestroy = new List<BaseBlock>(model.Blocks);
+
+            foreach (var b in toDestroy)
+            {
+                if (b != null) UnityEngine.Object.Destroy(b.gameObject);
+            }
+
+            while (model.Blocks.Count > 0)
+                model.RemoveBlock(model.Blocks[0].blockId);
+            while (model.Connections.Count > 0)
+                model.RemoveConnectionAt(0);
+
+            await UniTask.Yield();
+
+            var sceneRoot = UnityEngine.Object.FindObjectOfType<Views.GraphView>();
+            var parent = sceneRoot != null ? sceneRoot.BlockParent : null;
+
+            var factory = new BlockFactoryService();
+
+            foreach (var bd in data.blocks)
+            {
+                var block = factory.Create(bd.blockType, parent);
+                if (block == null) continue;
+
+                block.blockId = bd.blockId;
+                block.name = bd.gameObjectName;
+                block.transform.position = bd.position.ToVector3();
+                ApplyProps(block, bd.propertiesJson);
+                model.AddBlock(block);
+            }
+
+            foreach (var cd in data.connections)
+                model.AddConnection(new BlockConnection(
+                    cd.fromBlockId, cd.fromPortName, cd.toBlockId, cd.toPortName));
+
+            foreach (var rod in data.runtimeObjects)
+            {
+                Enum.TryParse<PrimitiveType>(rod.primitiveType, out var prim);
+                var go = GameObject.CreatePrimitive(prim);
+                go.name = rod.name;
+                go.transform.position = rod.position.ToVector3();
+                go.transform.eulerAngles = rod.rotation.ToVector3();
+                go.transform.localScale = rod.scale.ToVector3();
+                model.RegisterRuntimeObject(go);
+            }
+        }
+
         private static string SerializeProps(BaseBlock block) => block switch
         {
             SpawnObjectBlock b => JsonUtility.ToJson(new SpawnObjectProps
@@ -42,7 +146,7 @@ namespace _Scripts.Serialization
                 defaultA = b.defaultA, defaultB = b.defaultB, compareOp = b.compareOp.ToString()
             }),
             BranchBlock b => JsonUtility.ToJson(new BranchProps { defaultCondition = b.defaultCondition }),
-            LogBlock    b => JsonUtility.ToJson(new LogProps
+            LogBlock b => JsonUtility.ToJson(new LogProps
             {
                 defaultMessage = b.defaultMessage, logFloatValue = b.logFloatValue
             }),
@@ -59,28 +163,36 @@ namespace _Scripts.Serialization
                     var p = JsonUtility.FromJson<SpawnObjectProps>(json);
                     Enum.TryParse(p.primitiveType, out b.primitiveType);
                     b.objectName = p.objectName;
-                    b.defaultX = p.defaultX; b.defaultY = p.defaultY; b.defaultZ = p.defaultZ;
+                    b.defaultX = p.defaultX;
+                    b.defaultY = p.defaultY;
+                    b.defaultZ = p.defaultZ;
                     break;
                 }
                 case MoveObjectBlock b:
                 {
                     var p = JsonUtility.FromJson<MoveObjectProps>(json);
                     Enum.TryParse(p.mode, out b.mode);
-                    b.defaultX = p.defaultX; b.defaultY = p.defaultY; b.defaultZ = p.defaultZ;
+                    b.defaultX = p.defaultX;
+                    b.defaultY = p.defaultY;
+                    b.defaultZ = p.defaultZ;
                     break;
                 }
                 case RotateObjectBlock b:
                 {
                     var p = JsonUtility.FromJson<RotateObjectProps>(json);
                     Enum.TryParse(p.mode, out b.mode);
-                    b.defaultPitch = p.defaultPitch; b.defaultYaw = p.defaultYaw; b.defaultRoll = p.defaultRoll;
+                    b.defaultPitch = p.defaultPitch;
+                    b.defaultYaw = p.defaultYaw;
+                    b.defaultRoll = p.defaultRoll;
                     break;
                 }
                 case ScaleObjectBlock b:
                 {
                     var p = JsonUtility.FromJson<ScaleObjectProps>(json);
                     Enum.TryParse(p.mode, out b.mode);
-                    b.defaultX = p.defaultX; b.defaultY = p.defaultY; b.defaultZ = p.defaultZ;
+                    b.defaultX = p.defaultX;
+                    b.defaultY = p.defaultY;
+                    b.defaultZ = p.defaultZ;
                     break;
                 }
                 case VariableBlock b:
@@ -94,7 +206,8 @@ namespace _Scripts.Serialization
                 case CompareBlock b:
                 {
                     var p = JsonUtility.FromJson<CompareProps>(json);
-                    b.defaultA = p.defaultA; b.defaultB = p.defaultB;
+                    b.defaultA = p.defaultA;
+                    b.defaultB = p.defaultB;
                     Enum.TryParse(p.compareOp, out b.compareOp);
                     break;
                 }
@@ -108,7 +221,7 @@ namespace _Scripts.Serialization
                 {
                     var p = JsonUtility.FromJson<LogProps>(json);
                     b.defaultMessage = p.defaultMessage;
-                    b.logFloatValue  = p.logFloatValue;
+                    b.logFloatValue = p.logFloatValue;
                     break;
                 }
             }
